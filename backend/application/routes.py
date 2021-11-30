81bd349c161b67faaecb01fd15ac3836ed73046c
Harry Volker <57865118+htr-volker@users.noreply.github.com>
2021-11-30 21:11:38 +0000

    Adding create functionality for planets (#1)
    * frontend queries for planets and its moons
    
    * added create planet form

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -1,22 +1,15 @@
 from application import app, db
 from application.models import Planet, Moon
 from flask import request, jsonify
-from os import getenv
-
-print(f"CREATE_SCHEMA = {getenv('CREATE_SCHEMA')}")
-if getenv("CREATE_SCHEMA").lower() == "true":
-    print("Creating table schema")
-    db.drop_all()
-    db.create_all()
 
 @app.route('/create/planet', methods=["POST"])
 def create_planet():
-    data = request.json
+    json = request.json
     new_planet = Planet(
-        name = data["name"],
-        mass = data["mass"],
-        type = data["type"],
-        star_system = data["star_system"]
+        name = json["name"],
+        mass = json["mass"],
+        type = json["type"],
+        star_system = json["star_system"]
     )
     db.session.add(new_planet)
     db.session.commit()
@@ -24,11 +17,11 @@ def create_planet():
 
 @app.route('/create/moon/<int:planet_id>', methods=["POST"])
 def create_moon(planet_id):
-    data = request.json
+    json = request.json
     new_moon = Moon(
-        name = data["name"],
+        name = json["name"],
         planet_id = planet_id,
-        mass = data["mass"]
+        mass = json["mass"]
     )
     db.session.add(new_moon)
     db.session.commit()
@@ -39,13 +32,39 @@ def get_all_planets():
     all_planets = Planet.query.all()
     json = {"planets": []}
     for planet in all_planets:
+        moons = []
+        for moon in planet.moons:
+            moons.append(
+                {
+                    "id": moon.id,
+                    "name": moon.name,
+                    "planet_id": moon.planet_id,
+                    "mass": moon.mass
+                }
+            )
         json["planets"].append(
             {
                 "id": planet.id,
                 "name": planet.name,
                 "mass": planet.mass,
                 "type": planet.type,
-                "star_system": planet.star_system
+                "star_system": planet.star_system,
+                "moons": moons
+            }
+        )
+    return jsonify(json)
+
+@app.route('/get/allMoons', methods=["GET"])
+def get_all_moons():
+    all_moons = Planet.query.all()
+    json = {"moons": []}
+    for moon in all_moons:
+        json["moons"].append(
+            {
+                "id": moon.id,
+                "name": moon.name,
+                "planet_id": moon.planet_id,
+                "mass": moon.mass
             }
         )
     return jsonify(json)
@@ -78,7 +97,7 @@ def get_moons(id):
         )
     return jsonify(json)
 
-@app.route('/update/planet/<int:id>', methods=["POST"])
+@app.route('/update/planet/<int:id>', methods=["PUT"])
 def update_planet(id):
     data = request.json
     planet = Planet.query.get(id)
---
+++ frontend/application/forms.py
@@ -0,0 +1,17 @@
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+
+class CreatePlanetForm(FlaskForm):
+    name = StringField('Planet Name', validators=[DataRequired()])
+    mass = IntegerField('Planet Mass', validators=[DataRequired()])
+    type = SelectField('Planet Type', validators=[DataRequired()],
+        choices=[
+            ('Dwarf', 'Dwarf'),
+            ('Terrestrial', 'Terrestrial'),
+            ('Ice Giant', 'Ice Giant'),
+            ('Gas Giant', 'Gas Giant')
+        ]
+    )
+    star_system = StringField('Star System Name', validators=[DataRequired()])
+    submit = SubmitField('Add Planet')
